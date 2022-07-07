88eb8a7d2fcc8ef41744a7898a5baeb30c637be1
Nguyen Lam Nhat Tien <tien20001810@gmail.com>
2022-07-08 00:11:26 +0700

    init & finish

---
+++ TestLogging/Middleware/RequestResponseLoggingMiddleware.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using log4net;
+using Microsoft.AspNetCore.Http;
+
+namespace TestLogging.Middleware
+{
+    public class RequestResponseLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILog _logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        
+        private static string TraceId;
+
+        public RequestResponseLoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        private static string GenerateRequestTraceId()
+        {
+            return Guid.NewGuid().ToString();
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            TraceId = GenerateRequestTraceId();
+            
+            _logger.Info("Request" + Environment.NewLine
+                                             + "============================================" + Environment.NewLine
+                                             + "Host: " + context.Request.Host + Environment.NewLine
+                                             + "Path: " + context.Request.Path + Environment.NewLine
+                                             + "Method: " + context.Request.Method + Environment.NewLine
+                                             + "Protocol: " + context.Request.Protocol + Environment.NewLine
+                                             + "TraceId: " + TraceId + Environment.NewLine
+                                             + "============================================");
+
+            await _next.Invoke(context);
+
+            _logger.Info("Response" + Environment.NewLine
+                                    + "============================================" + Environment.NewLine
+                                    + "Host: " + context.Request.Host + Environment.NewLine
+                                    + "Path: " + context.Request.Path + Environment.NewLine
+                                    + "Method: " + context.Request.Method + Environment.NewLine
+                                    + "Protocol: " + context.Request.Protocol + Environment.NewLine
+                                    + "Status code: " + context.Response.StatusCode + Environment.NewLine
+                                    + "TraceId: " + TraceId + Environment.NewLine
+                                    + "============================================");
+        }
+    }
+}
